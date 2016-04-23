c0fbe199737e38cc4a1351754d9bd1d1b341d771
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-04-23 22:51:18 +0200

    Register Postback handler

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.post('/webhook', function (req, res) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
             }
+        } else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
         }
     }
     res.sendStatus(200);
